1b15811dfcc0a6f31fef81cc39f17652d961b7df
Christian Cmehil-Warn <github@christiancw.anonaddy.com>
2023-08-18 08:50:20 -0400

    Add Last ballots chart

--- pages/NPC_Election.py
+++ pages/NPC_Election.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-
+import numpy as np
 st.set_page_config(
     page_title="NPC Election",
 )
@@ -21,24 +21,41 @@ first_ballot = df.groupby('choice_1').count().reset_index()
 first_ballot = first_ballot.rename(columns={"choice_1":"Candidate", 'Voter':'Votes'})
 first_ballot = first_ballot[['Candidate', 'Votes']]
 
-last_ballot = df.groupby('choice_41').count().reset_index()
+final_ballot = df.groupby('choice_41').count().reset_index()
+final_ballot = final_ballot.rename(columns={"choice_41":"Candidate", 'Voter':'Votes'})
+final_ballot = final_ballot[['Candidate', 'Votes']]
+
+#Gets candidate ranked last on each ballot
+last_ballot = df.dropna(subset=['choice_1'])
+last_ballot = last_ballot.replace('', np.nan).fillna(axis=1, method='ffill')
+last_ballot = last_ballot.groupby('choice_41').count().reset_index()
 last_ballot = last_ballot.rename(columns={"choice_41":"Candidate", 'Voter':'Votes'})
 last_ballot = last_ballot[['Candidate', 'Votes']]
+st.write(last_ballot)
 
 #VISUAL ELEMENTS
 
 st.write("# NPC Election 2023")
+st.warning('Ballot weights are not considered yet', icon="⚠️")
 st.write("## Candidates Ranked #1")
 st.bar_chart(first_ballot, x="Candidate", y="Votes")
 
 with st.expander("Raw First Ballot Counts"):
     st.dataframe(first_ballot)
 
-st.write("## Candidates Ranked #41")
+
+st.write("## Candidates Ranked Last")
 st.bar_chart(last_ballot, x="Candidate", y="Votes")
 
 with st.expander("Raw Last Ballot Counts"):
     st.dataframe(last_ballot)
+
+st.write("## Candidates Ranked #41")
+st.write("Only ballots that ranked all candidates are counted for this chart")
+st.bar_chart(final_ballot, x="Candidate", y="Votes")
+
+with st.expander("Raw Ranked #41 Ballot Counts"):
+    st.dataframe(final_ballot)
 
 
 st.write("## Raw Ballots")
